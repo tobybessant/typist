d7e3d92827119a87336e1153071218b7e269ff06
Toby Bessant <toby.bessant.1998@gmail.com>
2019-11-25 23:21:47 +0000

    Base express setup in place
    Initial endpoint up and running

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 const dotenv = require("dotenv")
+const express = require("express")
 
 /* setup */
 dotenv.config()
+const PORT = process.env.PORT
+
+const app = express()
+
+app.get("/", (req, res) => {
+	res.json({ message: "Index endpoint reached" })
+})
+
+app.listen(PORT, () => {
+	console.log(`Server listening on ${PORT}. . .`)
+})
