e3c0e6639f63d1a87e6a6df5b2ac2d35f698f46d
Toby Bessant <toby.bessant.1998@gmail.com>
2019-11-26 00:12:14 +0000

    Installed express middleware and testing frameworks

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const dotenv = require("dotenv")
 const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
 
-/* setup */
+// environment config
 dotenv.config()
 const PORT = process.env.PORT
 
+// app config
 const app = express()
+app.use(bodyParser.json())
+app.use(morgan("dev"))
 
+// app routes
 app.get("/", (req, res) => {
 	res.json({ message: "Index endpoint reached" })
 })
 
+// app start
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}. . .`)
 })
