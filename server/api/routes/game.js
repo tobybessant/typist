e045eb6b5da2cfeb2cc14d99987cb0fd587fe128
Toby Bessant <toby.bessant.1998@gmail.com>
2019-12-01 00:05:45 +0000

    Added whitespace

--- server/api/routes/game.js
+++ server/api/routes/game.js
@@ -1,7 +1,7 @@
 const gameRouter = require("express").Router()
 const GameController = require("../controllers/gameController")
-
 const gameModel = require("../models/game")
+
 const gameController = new GameController(gameModel)
 
 gameRouter.post("/new", async (req, res) => gameController.createGame(req, res))
