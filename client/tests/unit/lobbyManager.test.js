30373d9ffe6790054a78d09b8af20ea8525b6a6a
Toby Bessant <toby.bessant.1998@gmail.com>
2019-12-06 13:42:07 +0000

    Refactored tests
    Nested suites to make console output clearer, fixed dysfunctional tests.

--- client/tests/unit/lobbyManager.test.js
+++ client/tests/unit/lobbyManager.test.js
@@ -4,113 +4,135 @@ const LobbyManager = require("../../src/services/lobbyManager")
 
 suite("Unit Tests :: Lobby Manager\n", () => {
 
-	let lobbyManager, hostName
+	let lobby, host, newPlayer
+
+	teardown(() => {
+		lobby = null
+		host = null
+		newPlayer = null
+	})
 
 	suite("Initialising a new lobby manager", () => {
 
-		teardown(() => {
-			lobbyManager = null
-			hostName = null
+		suite("Valid data", () => {
+			setup(() => {
+				host = "Toby"
+				lobby = new LobbyManager(host)
+			})
+
+			test("Sets the host", async () => {
+				assert.propertyVal(lobby, "host", host, "Host name not set correctly")
+			})
+
+			test("The connectedPlayers array is initialised as empty", () => {
+				assert.isArray(lobby.connectedPlyers, "connectedPlayers is not an array")
+			})
+
+			test("Host is added to the connectedPlayers array", () => {
+				assert.lengthOf(lobby.connectedPlyers, 1, "connectedPlayers is not length 1")
+				assert.include(lobby.connectedPlyers, host, "connectedPlayers does not contain the host")
+			})
 		})
 
-		test("Sets the host given valid data", async () => {
-			hostName = "Toby"
-			lobbyManager = new LobbyManager(hostName)
-			assert.propertyVal(lobbyManager, "host", hostName, "Host name not set correctly")
-		})
+		suite("Invalid data", () => {
+			setup(() => {
+				host = null
+			})
 
-		test("Throws an error when constructed with invalid host name", async () => {
-			hostName = null
-			assert.throws((hostName) => new LobbyManager(hostName), "Null host exception", "Null host exception not thrown")
-		})
-
-		test("The connected players array is initialised as empty", () => {
-			hostName = "Harold"
-			lobbyManager = new LobbyManager(hostName)
-
-			assert.isArray(lobbyManager.connectedPlyers, "connectedPlayers is not an array")
-		})
-
-		test("Host is added to the connectedPlayers array", () => {
-			hostName = "Lucy"
-			lobbyManager = new LobbyManager(hostName)
-
-			assert.lengthOf(lobbyManager.connectedPlyers, 1, "connectedPlayers is not length 1")
-			assert.include(lobbyManager.connectedPlyers, hostName, "connectedPlayers does not contain the host")
+			test("Throws an error when constructed with invalid host name", async () => {
+				host = null
+				assert.throws((hostName) => new LobbyManager(hostName), "Null host exception", "Null host exception not thrown")
+			})
 		})
 
 	})
 
 	suite("Adding a new player", () => {
-		let newPlayer
+		suite("Valid data", () => {
+			setup(() => {
+				host = "Toby"
+				newPlayer = "John"
+				lobby = new LobbyManager(host)
+			})
 
-		setup(() => {
-			hostName = "Toby"
-			lobbyManager = new LobbyManager(hostName)
+			test("Valid player is added to connected players array", async () => {
+				lobby.join(newPlayer)
+
+				assert.include(lobby.connectedPlyers, newPlayer, "connectedPlayers does not contain the new player")
+			})
+
+			test("Number of connected players increases by 1", async () => {
+				let initialCount = lobby.connectedPlyers.length
+				lobby.join(newPlayer)
+
+				assert(lobby.connectedPlyers.length === ++initialCount)
+			})
 		})
 
-		teardown(() => {
-			lobbyManager = null
-			hostName = null
-			newPlayer = null
-		})
+		suite("Invalid data", () => {
 
-		test("Valid player is added to connected players array", async () => {
-			newPlayer = "John"
-			lobbyManager.join(newPlayer)
-			assert.include(lobbyManager.connectedPlyers, hostName, "connectedPlayers does not contain the new player")
-		})
+			setup(() => {
+				host = "Toby"
+				newPlayer = null
+				lobby = new LobbyManager(host)
+			})
 
-		test("Number of connected players increases by 1", async () => {
-			newPlayer = "John"
-			assert.increasesBy(lobbyManager.join(newPlayer), lobbyManager.connectedPlyers.length, 1, "Number of connected players does not increase by 1")
-		})
+			test("Adding an invalid player throws an error", async () => {
+				assert.throws((newPlayer) => lobby.join(newPlayer), "Null player exception")
+			})
 
-		test("Invalid player is not added to connected players array", async () => {
-			newPlayer = null
-			lobbyManager.join(newPlayer)
-			assert.notInclude(lobbyManager.connectedPlyers, hostName, "connectedPlayers contains the new player")
-		})
+			test("Invalid player is not added to connected players array", async () => {
+				assert.throws((newPlayer) => lobby.join(newPlayer), "Null player exception")
+				assert.notInclude(lobby.connectedPlyers, newPlayer)
+			})
 
-		test("Number of connected players does not increase", async () => {
-			newPlayer = null
-			assert.doesNotIncrease(lobbyManager.join(newPlayer), lobbyManager.connectedPlyers.length, "Number of connected players increases")
+			test("Number of connected players does not increase", async () => {
+				let initialCount = lobby.connectedPlyers.length
+
+				assert.throws((newPlayer) => lobby.join(newPlayer), "Null player exception")
+				assert(initialCount === lobby.connectedPlyers.length, "Number of connected players increases")
+			})
 		})
 	})
 
 	suite("Removing a player", () => {
-		let newPlayer
 
-		setup(() => {
-			hostName = "Toby"
-			newPlayer = "John"
-			lobbyManager = new LobbyManager(hostName)
-			lobbyManager.join(newPlayer)
+		suite("Valid Data", () => {
+			setup(() => {
+				host = "Toby"
+				newPlayer = "John"
+				lobby = new LobbyManager(host)
+				lobby.join(newPlayer)
+			})
+
+			test("Number of connected players decreases by 1", async () => {
+				let initialCount = lobby.connectedPlyers.length
+				lobby.leave(newPlayer)
+
+				assert(lobby.connectedPlyers.length === --initialCount)
+			})
+
+			test("Leaving player is no longer in the connectedPlayers array", async () => {
+				lobby.leave(newPlayer)
+				assert.notInclude(lobby.connectedPlyers, newPlayer, "Leaving player is still a connectedPlayer")
+			})
 		})
+		suite("Invalid data", () => {
 
-		teardown(() => {
-			lobbyManager = null
-			hostName = null
-			newPlayer = null
-		})
+			setup(() => {
+				host = "Toby"
+				newPlayer = null
+				lobby = new LobbyManager(host)
+			})
 
-		test("Number of connected players decreases by 1", async () => {
-			assert.decreasesBy(lobbyManager.leave(newPlayer), lobbyManager.connectedPlyers.length, "Number of connected players does not decrease by 1")
-		})
+			test("Throws an error if the leaving player is null", async () => {
+				assert.throws((newPlayer) => lobby.leave(newPlayer), "Null player exception", "Null player exception not thrown")
+			})
 
-		test("Leaving player is no longer in the connectedPlayers list", async () => {
-			lobbyManager.leave(newPlayer)
-			assert.notInclude(lobbyManager.connectedPlyers, newPlayer, "Leaving player is still a connectedPlayer")
-		})
-
-		test("Throws an error if the leaving player is null", async () => {
-			newPlayer = null
-			assert.throws((newPlayer) => lobbyManager.leave(newPlayer), "Null player exception", "Null player exception not thrown")
-		})
-
-		test("Does not throw an error if the leaving player is not in the lobby", async () => {
-			newPlayer = "Joanne"
-			assert.doesNotThrow((newPlayer) => lobbyManager.leave(newPlayer), "Null player exception is thrown")
+			test("Does not throw an error if the leaving player is not in the lobby", async () => {
+				newPlayer = "Joanne"
+				assert.doesNotThrow((newPlayer) => lobby.leave(newPlayer), "Null player exception is thrown")
+			})
 		})
 	})
 })
