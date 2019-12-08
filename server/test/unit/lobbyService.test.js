0e5aa10bb0a758e10b36f2d02fb2d1e31000dbec
Toby Bessant <toby.bessant.1998@gmail.com>
2019-12-08 15:53:05 +0000

    LobbyController test updates
    Amended tests to expect lobbycontroller lobbyList to be a map using id's to remove.
    
    Added getLobby tests to search the lobbycontroller lobby list using a  given id

--- server/test/unit/lobbyService.test.js
+++ server/test/unit/lobbyService.test.js
@@ -4,10 +4,11 @@ const MockLobby = require("../mocks/models/lobby")
 const LobbyService = require("../../api/controllers/lobbyController")
 
 suite("Unit Tests :: Lobby Service", () => {
-	let lobbyService, lobby, id
+	let lobbyService, lobby, id, searchId
 
 	teardown(() => {
 		id = null
+		searchId = null
 		lobby = null
 		lobbyService = null
 	})
@@ -68,22 +69,68 @@ suite("Unit Tests :: Lobby Service", () => {
 		})
 	})
 
+	suite("Fetch a lobby", () => {
+		suite("Valid data", () => {
+			setup(() => {
+				id = "XYZA"
+				lobbyService = new LobbyService()
+				lobby = new MockLobby(id)
+
+				lobbyService.addLobby(lobby)
+			})
+
+			test("Getting a lobby returns the correct lobby", () => {
+				const fectchedLobby = lobbyService.getLobby(lobby.id)
+				assert(fectchedLobby.id, lobby.id)
+			})
+		})
+
+		suite("Invalid data [ Trying to get a lobby that does not exist in the list ]", () => {
+			setup(() => {
+				id = "XYZA"
+				lobbyService = new LobbyService()
+				lobby = new MockLobby(id)
+
+				lobbyService.addLobby(lobby)
+				searchId = "ABCD"
+			})
+
+			test("Throws lobby does not exist error", () => {
+				assert.throws((searchId) => lobbyService.getLobby(searchId), "Lobby does not exist")
+			})
+		})
+
+		suite("Invalid data [ lobby ID is null ]", () => {
+			setup(() => {
+				lobbyService = new LobbyService()
+				lobby = new MockLobby(id)
+			})
+
+			test("Throws lobby does not exist error", () => {
+				assert.throws(() => lobbyService.getLobby(null), "Lobby does not exist")
+			})
+		})
+	})
+
 	suite("Removing a lobby", () => {
 		suite("Valid data", () => {
 			setup(() => {
 				id = "XYZA"
 				lobbyService = new LobbyService()
 				lobby = new MockLobby(id)
+
+				lobbyService.addLobby(lobby)
 			})
 
 			test("Removig a lobby removes the lobby from the list", () => {
-				lobbyService.removeLobby(lobby)
+				lobbyService.removeLobby(lobby.id)
 				assert.notProperty(lobbyService.activeLobbies, lobby.id)
 			})
 
 			test("Removing a lobby decreases the lobby count by 1", () => {
 				const initialCount = Object.keys(lobbyService.activeLobbies).length
-				lobbyService.removeLobby(lobby)
+
+				lobbyService.removeLobby(lobby.id)
 				assert(Object.keys(lobbyService.activeLobbies).length === (initialCount - 1))
 			})
 		})
@@ -95,12 +142,12 @@ suite("Unit Tests :: Lobby Service", () => {
 			})
 
 			test("An error is not thrown", () => {
-				assert.doesNotThrow((lobby) => lobbyService.addLobby(lobby))
+				assert.doesNotThrow(() => lobbyService.removeLobby(null))
 			})
 
 			test("The lobby list is unaffected", () => {
 				const initialList = lobbyService.activeLobbies
-				lobbyService.removeLobby(lobby)
+				lobbyService.removeLobby(lobby.id)
 				assert(lobbyService.activeLobbies === initialList)
 			})
 		})
