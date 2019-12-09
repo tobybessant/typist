a0c9a1279c20f0ce2e38f29c0a4e9ec24b6279c8
Toby Bessant <toby.bessant.1998@gmail.com>
2019-12-09 09:27:44 +0000

    Implemented lobby controller and player model to pass tests

--- server/api/controllers/lobbyController.js
+++ server/api/controllers/lobbyController.js
@@ -1,3 +1,31 @@
 module.exports = class LobbyService {
+	constructor() {
+		this.activeLobbies = {}
+	}
 
+	addLobby(lobby) {
+		if (lobby && lobby.id) {
+			this.activeLobbies[lobby.id] = lobby
+		} else {
+			throw new Error("Malformed lobby")
+		}
+	}
+
+	removeLobby(id) {
+		if (this.activeLobbies[id]) {
+			delete this.activeLobbies[id]
+		}
+	}
+
+	getLobby(id) {
+		if (!id) {
+			throw new Error("Invalid search ID")
+		}
+
+		if (!this.activeLobbies[id]) {
+			throw new Error("Lobby does not exist")
+		}
+
+		return this.activeLobbies[id]
+	}
 }
